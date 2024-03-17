8f18f82730521940a50a387f4f1170d4ce1e4a65
CursedSliver <162417685+CursedSliver@users.noreply.github.com>
2024-03-17 11:44:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -575,9 +575,6 @@ Game.registerMod("Kaizo Cookies", {
 		Game.veilOpacity = function() {
 			return Math.pow(Game.veilHP / Game.veilMaxHP, 0.25)
 		}
-		Game.veilParticleFailChance = function() {
-			return 1 - Math.pow(Game.veilHP / Game.veilMaxHP, 0.25);
-		}
 		Game.veilRevolveFactor = function() {
 			return 0.06 * Math.pow(Game.veilHP / Game.veilMaxHP, 0.5);
 		}
@@ -590,8 +587,7 @@ Game.registerMod("Kaizo Cookies", {
 		veilDraw = veilDraw.replace('ctx.globalAlpha=1;', 'ctx.globalAlpha=Game.veilOpacity();');
 		veilDraw = veilDraw.replace("ctx.globalCompositeOperation='source-over';", "ctx.globalAlpha = 1; ctx.globalCompositeOperation='source-over';");
 		veilDraw = veilDraw.replace('for (i=0;i<6;i++)', 'for (i=0;i<9;i++)');
-		veilDraw = veilDraw.replace('var t=Game.T+i*15;', 'var t=Game.T+i*10;')
-		veilDraw = veilDraw.replace("ctx.drawImage(Pic('glint.png'),xx-size/2,yy-size/2,size,size);", "if (Math.random() > Game.veilParticleFailChance()) { ctx.drawImage(Pic('glint.png'),xx-size/2,yy-size/2,size,size); }")
+		veilDraw = veilDraw.replace('var t=Game.T+i*15;', 'var t=Game.T+i*10;');
 		veilDraw = veilDraw.replace('var a=(Math.floor(t/30)*30*6-i*30)*0.01;', 'var a=(Math.floor(t/30)*30*6-i*30)*Game.veilRevolveFactor();');
 		veilDraw = veilDraw.replace('var size=32*(1-Math.pow(r*2-1,2));', 'var size=Game.veilParticleSizeMax()*(1-Math.pow(r*2-1,2));');
 		veilDraw = veilDraw.replace('var xx=x+Math.sin(a)*(110+r*16);', 'var xx=x+Math.sin(a)*(110+r*Game.veilParticleSpeed());').replace('var yy=y+Math.cos(a)*(110+r*16);', 'var yy=y+Math.cos(a)*(110+r*Game.veilParticleSpeed());');
