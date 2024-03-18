2a21ee5ecb522ee4bde10366e72d2022b03f3d53
CursedSliver <162417685+CursedSliver@users.noreply.github.com>
2024-03-18 10:26:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1298,13 +1298,16 @@ Game.registerMod("Kaizo Cookies", {
 						Game.Upgrades['Shimmering veil [off]'].earn();
 						Game.Lock('Shimmering veil [on]'); Game.Unlock('Shimmering veil [on]'); 
 						Game.Lock('Shimmering veil [broken]');
+						console.log('veil on!');
 					} else if (strIn[1] == 'off') {
 						Game.Upgrades['Shimmering veil [on]'].earn();
 						Game.Lock('Shimmering veil [off]'); Game.Unlock('Shimmering veil [off]'); 
 						Game.Upgrades['Shimmering veil [broken]'].unlocked = 0;
+						console.log('veil off!');
 					} else {
 						Game.Lock('Shimmering veil [on]'); Game.Lock('Shimmering veil [off]');
 						Game.Upgrades['Shimmering veil [broken]'].earn();
+						console.log('veil broken!');
 					}
 
 				}
