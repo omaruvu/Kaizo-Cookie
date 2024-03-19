5d060688c020213296820528b83bf4e8791efa7c
CursedSliver <162417685+CursedSliver@users.noreply.github.com>
2024-03-19 11:40:19 +0800

    lets try

--- main.js
+++ main.js
@@ -560,6 +560,8 @@ Game.registerMod("Kaizo Cookies", {
 			if (Game.Objects['Wizard tower'].minigameLoaded && !grimoireUpdated) {
 				var gp = Game.Objects['Wizard tower'].minigame
 				var M = gp;
+				Game.rebuildGrimoire();
+				decay.addSpells();
 				eval('gp.logic='+gp.logic.toString().replace('M.magicPS=Math.max(0.002,Math.pow(M.magic/Math.max(M.magicM,100),0.5))*0.002;', 'M.magicPS = Math.pow(Math.min(2, decay.gen), 0.3) * Math.max(0.002,Math.pow(M.magic/Math.max(M.magicM,100),0.5))*0.006;'));
 				eval('gp.logic='+replaceAll('M.','gp.',gp.logic.toString()));
 				eval('gp.draw='+M.draw.toString().replace(`Math.min(Math.floor(M.magicM),Beautify(M.magic))+'/'+Beautify(Math.floor(M.magicM))+(M.magic<M.magicM?(' ('+loc("+%1/s",Beautify((M.magicPS||0)*Game.fps,2))+')'):'')`,
@@ -569,7 +571,6 @@ Game.registerMod("Kaizo Cookies", {
 				eval('gp.draw='+replaceAll('M.','gp.',gp.draw.toString()));		
 				eval('gp.spells["hand of fate"].win='+gp.spells["hand of fate"].win.toString().replace(`if (Game.BuildingsOwned>=10 && Math.random()<0.25) choices.push('building special');`, 'decay.triggerNotif("fthof");'));
 				eval('gp.spells["hand of fate"].fail='+gp.spells["hand of fate"].fail.toString().replace(`if (Math.random()<0.1) choices.push('cursed finger','blood frenzy');`, `if (Math.random()<0.1) choices.push('cursed finger'); decay.triggerNotif("fthof");`));
-				decay.addSpells();
 				grimoireUpdated = true; //no more unnecessary replacing 
 			}
 		});
@@ -940,6 +941,71 @@ Game.registerMod("Kaizo Cookies", {
 					Game.Popup('<div style="font-size:80%;">'+loc("Backfire! Corruption intensified!")+'</div>',Game.mouseX,Game.mouseY);
 				}
 			}
+		}
+
+		Game.rebuildGrimoire = function() {
+			if (typeof gp === 'undefined') { return false; }
+			let M = gp;
+			var str='';
+			str+='<style>'+
+			'#grimoireBG{background:url('+Game.resPath+'img/shadedBorders.png),url('+Game.resPath+'img/BGgrimoire.jpg);background-size:100% 100%,auto;position:absolute;left:0px;right:0px;top:0px;bottom:16px;}'+
+			'#grimoireContent{position:relative;box-sizing:border-box;padding:4px 24px;}'+
+			'#grimoireBar{max-width:95%;margin:4px auto;height:16px;}'+
+			'#grimoireBarFull{transform:scale(1,2);transform-origin:50% 0;height:50%;}'+
+			'#grimoireBarText{transform:scale(1,0.8);width:100%;position:absolute;left:0px;top:0px;text-align:center;color:#fff;text-shadow:-1px 1px #000,0px 0px 4px #000,0px 0px 6px #000;margin-top:2px;}'+
+			'#grimoireSpells{text-align:center;width:100%;padding:8px;box-sizing:border-box;}'+
+			'.grimoireIcon{pointer-events:none;margin:2px 6px 0px 6px;width:48px;height:48px;opacity:0.8;position:relative;}'+
+			'.grimoirePrice{pointer-events:none;}'+
+			'.grimoireSpell{box-shadow:4px 4px 4px #000;cursor:pointer;position:relative;color:#f33;opacity:0.8;text-shadow:0px 0px 4px #000,0px 0px 6px #000;font-weight:bold;font-size:12px;display:inline-block;width:60px;height:74px;background:url('+Game.resPath+'img/spellBG.png);}'+
+			'.grimoireSpell.ready{color:rgba(255,255,255,0.8);opacity:1;}'+
+			'.grimoireSpell.ready:hover{color:#fff;}'+
+			'.grimoireSpell:hover{box-shadow:6px 6px 6px 2px #000;z-index:1000000001;top:-1px;}'+
+			'.grimoireSpell:active{top:1px;}'+
+			'.grimoireSpell.ready .grimoireIcon{opacity:1;}'+
+			'.grimoireSpell:hover{background-position:0px -74px;} .grimoireSpell:active{background-position:0px 74px;}'+
+			'.grimoireSpell:nth-child(4n+1){background-position:-60px 0px;} .grimoireSpell:nth-child(4n+1):hover{background-position:-60px -74px;} .grimoireSpell:nth-child(4n+1):active{background-position:-60px 74px;}'+
+			'.grimoireSpell:nth-child(4n+2){background-position:-120px 0px;} .grimoireSpell:nth-child(4n+2):hover{background-position:-120px -74px;} .grimoireSpell:nth-child(4n+2):active{background-position:-120px 74px;}'+
+			'.grimoireSpell:nth-child(4n+3){background-position:-180px 0px;} .grimoireSpell:nth-child(4n+3):hover{background-position:-180px -74px;} .grimoireSpell:nth-child(4n+3):active{background-position:-180px 74px;}'+
+			
+			'.grimoireSpell:hover .grimoireIcon{top:-1px;}'+
+			'.grimoireSpell.ready:hover .grimoireIcon{animation-name:bounce;animation-iteration-count:infinite;animation-duration:0.8s;}'+
+			'.noFancy .grimoireSpell.ready:hover .grimoireIcon{animation:none;}'+
+			
+			'#grimoireInfo{text-align:center;font-size:11px;margin-top:12px;color:rgba(255,255,255,0.75);text-shadow:-1px 1px 0px #000;}'+
+			'</style>';
+			str+='<div id="grimoireBG"></div>';
+			str+='<div id="grimoireContent">';
+				str+='<div id="grimoireSpells">';//did you know adding class="shadowFilter" to this cancels the "z-index:1000000001" that displays the selected spell above the tooltip? stacking orders are silly https://philipwalton.com/articles/what-no-one-told-you-about-z-index/
+				for (var i in M.spells)
+				{
+					var me=M.spells[i];
+					var icon=me.icon||[28,12];
+					str+='<div class="grimoireSpell titleFont" id="grimoireSpell'+me.id+'" '+Game.getDynamicTooltip('Game.ObjectsById['+M.parent.id+'].minigame.spellTooltip('+me.id+')','this')+'><div class="usesIcon shadowFilter grimoireIcon" style="background-position:'+(-icon[0]*48)+'px '+(-icon[1]*48)+'px;"></div><div class="grimoirePrice" id="grimoirePrice'+me.id+'">-</div></div>';
+				}
+				str+='</div>';
+				var icon=[29,14];
+				str+='<div id="grimoireBar" class="smallFramed meterContainer" style="width:1px;"><div '+Game.getDynamicTooltip('Game.ObjectsById['+M.parent.id+'].minigame.refillTooltip','this')+' id="grimoireLumpRefill" class="usesIcon shadowFilter lumpRefill" style="left:-40px;top:-17px;background-position:'+(-icon[0]*48)+'px '+(-icon[1]*48)+'px;"></div><div id="grimoireBarFull" class="meter filling" style="width:1px;"></div><div id="grimoireBarText" class="titleFont"></div><div '+Game.getTooltip('<div style="padding:8px;width:300px;font-size:11px;text-align:center;">'+loc("This is your magic meter. Each spell costs magic to use.<div class=\"line\"></div>Your maximum amount of magic varies depending on your amount of <b>Wizard towers</b>, and their level.<div class=\"line\"></div>Magic refills over time. The lower your magic meter, the slower it refills.")+'</div>')+' style="position:absolute;left:0px;top:0px;right:0px;bottom:0px;"></div></div>';
+				str+='<div id="grimoireInfo"></div>';
+			str+='</div>';
+			div.innerHTML=str;
+			M.magicBarL=l('grimoireBar');
+			M.magicBarFullL=l('grimoireBarFull');
+			M.magicBarTextL=l('grimoireBarText');
+			M.lumpRefill=l('grimoireLumpRefill');
+			M.infoL=l('grimoireInfo');
+			for (var i in M.spells)
+			{
+				var me=M.spells[i];
+				AddEvent(l('grimoireSpell'+me.id),'click',function(spell){return function(){PlaySound('snd/tick.mp3');M.castSpell(spell);}}(me));
+			}
+			AddEvent(M.lumpRefill,'click',function(){
+				if (M.magic<M.magicM)
+				{Game.refillLump(1,function(){
+					M.magic+=100;
+					M.magic=Math.min(M.magic,M.magicM);
+					PlaySound('snd/pop'+Math.floor(Math.random()*3+1)+'.mp3',0.75);
+				});}
+			});
 		}
 
 		//other nerfs and buffs down below (unrelated but dont know where else to put them)
