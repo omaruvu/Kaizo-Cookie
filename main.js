11a9a9d738b2b07e511dbbf6a5add27550406990
CursedSliver <162417685+CursedSliver@users.noreply.github.com>
2024-03-25 19:58:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1859,7 +1859,7 @@ Game.registerMod("Kaizo Cookies", {
 				'News: No, Fractal engines can\'t replicate into a larger copy of itself, either.'
 			]); }
 
-			var characters = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','1','2','3','4','5','6','7','8','9','0','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']			var r = function() {
+			var characters = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','1','2','3','4','5','6','7','8','9','0','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'];
 			var r = function(num) {
 				var str = '';
 				for (let i = 0; i < num; i++) {
